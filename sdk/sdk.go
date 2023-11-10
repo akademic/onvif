5a829243486063c7052761d15153de4ac21a19a1
Akademic <akademic@hub.sknt.ru>
2023-11-10 20:47:24 +0300

    sdk: fixed typo

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -5,15 +5,12 @@ import (
 	"encoding/xml"
 	"io/ioutil"
 	"net/http"
-	"os"
-	"time"
 
 	"github.com/juju/errors"
-	"github.com/rs/zerolog"
 )
 
 func ReadAndParse(ctx context.Context, httpReply *http.Response, reply interface{}, tag string) error {
-	defaultLogger.Debug("action=%s msg="%s" status=%s", tag, httpReply.Status, httpReply.StatusCode)
+	defaultLogger.Debug("action=%s msg=\"%s\" status=%s", tag, httpReply.Status, httpReply.StatusCode)
 
 	b, err := ioutil.ReadAll(httpReply.Body)
 	if err != nil {
